670bbb17536f50b3d5ee1a756ed3dcf8b29ba398
skan-splunk <103170004+skan-splunk@users.noreply.github.com>
2022-07-13 22:47:30 +1000

    fixing incorrect signalflow for app count

--- charts/app_count/main.tf
+++ charts/app_count/main.tf
@@ -2,7 +2,7 @@ resource "signalfx_single_value_chart" "app_count" {
   name = "Number of Apps"
   description = "Returns the number of apps on ArgoCD"
   program_text = <<-EOF
-A = data('argocd_app_sync_total').count().publish(label='A')
+A = data('argocd_app_info').count().publish(label='A')
 EOF
 
 }
